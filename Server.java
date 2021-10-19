de4763af506f932c08f24dff235f7a517c03f082
Vicente Salcedo <vicentepsalcedo@gmail.com>
2021-10-19 12:45:00 -0400

    '

--- Server.java
+++ Server.java
@@ -11,10 +11,11 @@ public class Server {
         int port = Integer.parseInt(args[0]);
  
         try (ServerSocket serverSocket = new ServerSocket(port)) {
- 
+            long startTime = System.nanoTime();
             System.out.println("Server is listening on port " + port);
  
             while (true) {
+                
                 Socket socket = serverSocket.accept();
                 System.out.println("New client connected");
  
@@ -24,14 +25,56 @@ public class Server {
                 OutputStream output = socket.getOutputStream();
                 PrintWriter writer = new PrintWriter(output, true);
  
- 
+                String s = null;
                 String text;
  
                 do {
                     text = reader.readLine();
-                    String reverseText = new StringBuilder(text).reverse().toString();
-                    writer.println("Server: " + reverseText);
- 
+                    // parse option from client to integer for evaluation
+                    int option = Integer.parseInt(text);
+                    if (option == 1){
+                        writer.println("Server: " + text);
+                    } else if(option ==2 ){
+                        writer.println(new Date().toString());
+                    } else if(option == 3){
+                        long endTime = System.nanoTime();
+                        long duration = (endTime - startTime);
+                        writer.println(duration);
+                    } else if(option == 4){
+                        try {
+            
+                            // run the Unix "ps -ef" command
+                                // using the Runtime exec method:
+                                Process p = Runtime.getRuntime().exec("ps -ef");
+                                
+                                BufferedReader stdInput = new BufferedReader(new 
+                                     InputStreamReader(p.getInputStream()));
+                    
+                                BufferedReader stdError = new BufferedReader(new 
+                                     InputStreamReader(p.getErrorStream()));
+                    
+                                // read the output from the command
+                                System.out.println("Here is the standard output of the command:\n");
+                                while ((s = stdInput.readLine()) != null) {
+                                    System.out.println(s);
+                                }
+                                
+                                // read any errors from the attempted command
+                                System.out.println("Here is the standard error of the command (if any):\n");
+                                while ((s = stdError.readLine()) != null) {
+                                    System.out.println(s);
+                                }
+                                
+                                System.exit(0);
+                            }
+                            catch (IOException e) {
+                                System.out.println("exception happened - here's what I know: ");
+                                e.printStackTrace();
+                                System.exit(-1);
+                            }
+                    } else {
+                        writer.println("Wrong Answer." + "'" + text + "'");
+                    }
                 } while (!text.equals("bye"));
  
                 socket.close();
