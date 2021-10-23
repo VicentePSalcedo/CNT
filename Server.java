55bed99240f1862de9d9c1871922ce7039ed1474
Vicente Salcedo <vicentepsalcedo@gmail.com>
2021-10-23 13:55:13 -0400

    new commit

--- Server.java
+++ Server.java
@@ -1,60 +1,72 @@
-import java.io.*;
-import java.net.*;
 import java.util.Date;
- 
+import java.util.Scanner;
 
 public class Server {
  
     public static void main(String[] args) {
-        if (args.length < 1) return;
- 
-        int port = Integer.parseInt(args[0]);
- 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            long startTime = System.nanoTime();
-            System.out.println("Server is listening on port " + port);
- 
-            while (true) {
-                
-                Socket socket = serverSocket.accept();
-                System.out.println("New client connected");
- 
-                InputStream input = socket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
- 
-                OutputStream output = socket.getOutputStream();
-                PrintWriter writer = new PrintWriter(output, true);
- 
-                // String s = null;
-                String text;
- 
-                do {
-                    text = reader.readLine();
-                    // parse option from client to integer for evaluation
-                    int option = Integer.parseInt(text);
-                    if (option == 1){
-                        writer.println(new Date().toString());
-                    } else if(option ==2 ){
-                        long endTime = System.nanoTime();
-                        long duration = (endTime - startTime);
-                        writer.println(duration);
-                    } else if(option == 3){
-                        long UsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
-                        writer.println(UsedMem);
-                    } else if(option == 4){
-                        long UsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
-                        writer.println(UsedMem);
-                    } else {
-                        writer.println("Wrong Answer." + "'" + text + "'");
-                    }
-                } while (!text.equals("bye"));
- 
-                socket.close();
-            }
- 
-        } catch (IOException ex) {
-            System.out.println("Server exception: " + ex.getMessage());
-            ex.printStackTrace();
+        long startTime = System.nanoTime();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1.Date and Time\n2.Uptime\n3.Memory Use\n4.Netstat\n5.Current Users\n6.Running Processes\nPlease select an option by number:");
+        Integer option = scanner.nextInt();
+        System.out.println("Option Selected was: " + option);
+        
+       
+        String output = "Empty";
+        switch (option) {
+        case 1:
+            output = dateAndTime();
+            break;
+        case 2:
+            output = upTime(startTime);
+            break;
+        case 3:
+            output = memoryUse();
+            break;
+        case 4:
+            output = netStat();
+            break;
+        case 5:
+            output = currentUsers();
+            break;
+        case 6:
+            output = runningProcess();
+            break;
+        default:
+            System.out.println("Select Valid Option");
+            break;
         }
+        System.out.println(output);
+        scanner.close();
     }
+
+    static String dateAndTime(){
+        String date = new Date().toString();
+        return date;
+    }
+
+    static String upTime(long startTime){
+        String totalTimeString;
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        totalTimeString = Long.toString(totalTime);
+        return totalTimeString;
+    }
+    
+    static String memoryUse(){
+        return null;
+    }
+
+    static String netStat(){
+        return null;
+    }
+
+    static String currentUsers(){
+        return null;
+    }
+
+    static String runningProcess(){
+        return null;
+    }
+
 }
