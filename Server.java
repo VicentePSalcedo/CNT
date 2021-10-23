132bf0bf4e1e86cffe0a1879f340c1ca294579ae
Vicente Salcedo <vicentepsalcedo@gmail.com>
2021-10-23 16:35:47 -0400

    In theory, this is working server side code

--- Server.java
+++ Server.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -5,48 +13,58 @@ public class Server {
  
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("1.Date and Time\n2.Uptime\n3.Memory Use\n4.Netstat\n5.Current Users\n6.Running Processes\nPlease select an option by number:");
-        Integer option = scanner.nextInt();
-        System.out.println("Option Selected was: " + option);
-        
-       
-        String output = "Empty";
-        switch (option) {
-        case 1:
-            output = dateAndTime();
-            break;
-        case 2:
-            output = upTime(startTime);
-            break;
-        case 3:
-            output = memoryUse();
-            break;
-        case 4:
-            output = netStat();
-            break;
-        case 5:
-            output = currentUsers();
-            break;
-        case 6:
-            output = runningProcess();
-            break;
-        default:
-            System.out.println("Select Valid Option");
-            break;
+        try {
+            ServerSocket serverSocket = new ServerSocket(6868);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                
+                String userInputString = reader.readLine();
+                Integer userInpuInteger = Integer.parseInt(userInputString);
+                
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+                
+                String outputString;
+                switch (userInpuInteger) {
+                    case 1:
+                        outputString = dateAndTime();
+                        break;
+                    case 2:
+                        outputString = upTime(startTime);
+                        break;
+                    case 3:
+                        outputString = memoryUse();
+                        break;
+                    case 4:
+                        outputString = netStat();
+                        break;
+                    case 5:
+                        outputString = currentUsers();
+                        break;
+                    case 6:
+                        outputString = runningProcess();
+                        break;
+                    default:
+                        outputString = "Something went wrong on the server side";
+                        break;
+                }
+                writer.println(outputString);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println(output);
-        scanner.close();
     }
 
     static String dateAndTime(){
-        String date = new Date().toString();
+        String date = "";
+        date = new Date().toString();
         return date;
     }
 
     static String upTime(long startTime){
-        String totalTimeString;
+        String totalTimeString = "";
         long endTime   = System.nanoTime();
         long totalTime = endTime - startTime;
         totalTimeString = Long.toString(totalTime);
@@ -54,19 +72,46 @@ public class Server {
     }
     
     static String memoryUse(){
-        return null;
+        String usedMemString = "";
+        long usedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+        usedMemString = Long.toString(usedMem);
+        return usedMemString;
     }
 
     static String netStat(){
-        return null;
+        String currentLine = "";
+        String netStatString = "";
+        try {
+            Process NetStat = Runtime.getRuntime().exec("netstat");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(NetStat.getInputStream()));
+            while ((currentLine = reader.readLine()) != null) {
+                netStatString = netStatString + currentLine + "\n";
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return netStatString;
     }
 
     static String currentUsers(){
-        return null;
+        return "5 is not setup yet";
     }
 
     static String runningProcess(){
-        return null;
+        String currentLine = "";
+        String runningProcessString = "";
+        try {
+            Process RunningProcess = Runtime.getRuntime().exec("tasklist");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(RunningProcess.getInputStream()));
+            while ((currentLine = reader.readLine()) != null){
+                runningProcessString = runningProcessString + currentLine + "\n";
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return runningProcessString;
     }
 
 }
