9626cf8ba4c619e3a5ce151486d31f8a84a6abe4
Vicente Salcedo <vicentepsalcedo@gmail.com>
2021-10-26 17:12:59 -0400

    Squashed commit of the following:
    commit 1c2565575099397c48cc4c492cf9f82bba43d7cf
    Author: Vicente Salcedo <vicentepsalcedo@gmail.com>
    Date:   Tue Oct 26 17:10:28 2021 -0400
    
        Finalized Client Side iteration
    
    commit fc940e6388be92fe221f1ce8531d0b336124cef9
    Author: Vicente Salcedo <vicentepsalcedo@gmail.com>
    Date:   Tue Oct 26 16:29:07 2021 -0400
    
        Most up to date client side commit
    
    commit e0b0e15375a78bd12f8e82bc0c5ade4618871856
    Author: Vicente Salcedo <vicentepsalcedo@gmail.com>
    Date:   Tue Oct 26 15:55:43 2021 -0400
    
        Packaged Option Logic
    
    commit 00ed1a7cbd84c5f4094c1e8ae981312662c8f9de
    Author: Vicente Salcedo <vicentepsalcedo@gmail.com>
    Date:   Mon Oct 25 18:02:37 2021 -0400
    
        Manual testing ground works for options "1-3" the rest is still broken
    
    commit ad543b2421552c5e9da70f7c02b842f771eab9ed
    Author: Vicente Salcedo <vicentepsalcedo@gmail.com>
    Date:   Sat Oct 23 21:44:09 2021 -0400
    
        the actual client side connection
    
    commit 73b0d795afbfada0850901f3e876f60163812c7e
    Author: Vicente Salcedo <vicentepsalcedo@gmail.com>
    Date:   Sat Oct 23 21:43:08 2021 -0400
    
        Input stream for client side connection

--- Client.java
+++ Client.java
@@ -1,7 +1,50 @@
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Client {
  
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        try {
+            Socket socket = new Socket("localhost", 6868);
+            
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+            
+            Console console = System.console();
+            String userInputString;
+            String serverOutpuString;
+            
+            userInputString = console.readLine("Select Option 1-6\n");
+
+            writer.println(userInputString);
+            
+            serverOutpuString = ReadBigString(reader);
+            
+            System.out.println(serverOutpuString);
+                
+            
+            socket.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static String ReadBigString(BufferedReader reader) throws IOException{
+        StringBuilder everything = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null){
+            everything.append(line + "\n");
+        }
+        return everything.toString();
     }
 }
--- Server.java
+++ Server.java
@@ -12,58 +12,76 @@ public class Server {
  
     public static void main(String[] args) {
         long startTime = System.nanoTime();
+        String serverOutputString;
         try {
             ServerSocket serverSocket = new ServerSocket(6868);
+            System.out.println("Server is listening on port 6868");
             while (true) {
                 Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+
                 InputStream input = socket.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                
-                String userInputString = reader.readLine();
-                Integer userInpuInteger = Integer.parseInt(userInputString);
-                
+
                 OutputStream output = socket.getOutputStream();
                 PrintWriter writer = new PrintWriter(output, true);
                 
-                String outputString;
-                switch (userInpuInteger) {
-                    case 1:
-                        outputString = dateAndTime();
-                        break;
-                    case 2:
-                        outputString = upTime(startTime);
-                        break;
-                    case 3:
-                        outputString = memoryUse();
-                        break;
-                    case 4:
-                        outputString = netStat();
-                        break;
-                    case 5:
-                        outputString = currentUsers();
-                        break;
-                    case 6:
-                        outputString = runningProcess();
-                        break;
-                    default:
-                        outputString = "Something went wrong on the server side";
-                        break;
-                }
-                writer.println(outputString);
+                
+                String userInputString;
+                Integer userInputInteger;
+
+                userInputString = reader.readLine();
+                    
+                userInputInteger = Integer.parseInt(userInputString);
+
+                serverOutputString = optionLogic(userInputInteger, startTime);
+
+                writer.println(serverOutputString);
+
                 socket.close();
+                System.out.println("Client disconected.");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    static String optionLogic(Integer userInputInteger, long startTime){
+        String output;
+        switch (userInputInteger) {
+            case 1:
+                output = dateAndTime();
+                break;
+            case 2:
+                output = upTime(startTime);
+                break;
+            case 3:
+                output = memoryUse();
+                break;
+            case 4:
+                output = netStat();
+                break;
+            case 5:
+                output = currentUsers();
+                break;
+            case 6:
+                output = runningProcess();
+                break;
+            default:
+                output = "Invalid Option";
+                break;
+        }
+        return output;
+    }
+    
     static String dateAndTime(){
+        System.out.println("Date and Time Requested");
         String date = "";
         date = new Date().toString();
         return date;
     }
 
     static String upTime(long startTime){
+        System.out.println("Uptime Requested");
         String totalTimeString = "";
         long endTime   = System.nanoTime();
         long totalTime = endTime - startTime;
@@ -72,6 +90,7 @@ public class Server {
     }
     
     static String memoryUse(){
+        System.out.println("Memory Use Requested");
         String usedMemString = "";
         long usedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
         usedMemString = Long.toString(usedMem);
@@ -91,14 +110,17 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Net Work Status requested");
         return netStatString;
     }
 
     static String currentUsers(){
+        System.out.println("Current Users Requested");
         return "5 is not setup yet";
     }
 
     static String runningProcess(){
+        System.out.println("Running Process");
         String currentLine = "";
         String runningProcessString = "";
         try {
