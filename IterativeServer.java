aa33f6e77819fa4ce212daaa5d2041a0b6096124
Vicente Salcedo <vicentepsalcedo@gmail.com>
2021-11-30 11:50:03 -0500

    Conccurent Commit

--- IterativeServer.java
+++ IterativeServer.java
@@ -1,4 +1,4 @@
-package WindowsServerDeployment;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,61 +9,44 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 
-public class Server {
+public class IterativeServer {
  
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
+        String serverOutputString;
         try {
             ServerSocket serverSocket = new ServerSocket(1313);
             System.out.println("Server is listening on port 1313");
             while (true) {
-
                 Socket socket = serverSocket.accept();
                 System.out.println("New client connected");
-                new ServerThread(socket,startTime).start();
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+                
+                String userInputString;
+                Integer userInputInteger;
+
+                userInputString = reader.readLine();
+                    
+                userInputInteger = Integer.parseInt(userInputString);
+
+                serverOutputString = optionLogic(userInputInteger, startTime);
+
+                writer.println(serverOutputString);
+                
+                socket.close();
+                System.out.println("Client disconected.");
             }
         } catch (IOException e) {
             System.out.println("An error occured while establishing a new client connection.");
             e.printStackTrace();
         }
     }
-}
-
-class ServerThread extends Thread {
-    Socket socket;
-    long startTime;
-    public ServerThread(Socket socket, long startTime){
-        this.socket = socket;
-        this.startTime = startTime;
-    }
-    public void run(){
-        String serverOutputString;
-        try{
-
-            InputStream input = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-            OutputStream output = socket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
-            
-            String userInputString;
-            Integer userInputInteger;
-
-            userInputString = reader.readLine();
-                
-            userInputInteger = Integer.parseInt(userInputString);
-
-            serverOutputString = optionLogic(userInputInteger, startTime);
-
-            writer.println(serverOutputString);
-            
-            socket.close();
-            System.out.println("Client disconected.");
-        } catch (IOException ex) {
-            System.out.println("An error occured while establishing a new client connection.");
-            ex.printStackTrace();
-        }
-    }
+    
     static String optionLogic(Integer userInputInteger, long startTime){
         String output;
         switch (userInputInteger) {
